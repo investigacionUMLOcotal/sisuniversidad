9c7909ce74ed44c030c0a275c5b851ca33a90748
Mario Zapata <mrzapata74@gmail.com>
2020-11-22 16:40:02 -0600

    Estilos para formulario Agregar Docente

--- docentes.view.php
+++ docentes.view.php
@@ -2,7 +2,7 @@
 <?php
 require 'functions.php';
 //Define queienes tienen permiso en este archivo
-$permisos = ['Administrador','Profesor'];
+$permisos = ['Administrador', 'Profesor'];
 permisos($permisos);
 //consulta las secciones
 $secciones = $conn->prepare("select * from secciones");
@@ -15,84 +15,92 @@ $grados->execute();
 $grados = $grados->fetchAll();
 ?>
 <html>
+
 <head>
-<title>Inicio | Registro </title>
+    <title>Inicio | Registro </title>
     <meta name="description" content="Registro  Sis Universidad Martin Lutero Quilali" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="css/style.css" />
+    <!-- <link rel="stylesheet" href="css/style.css" /> -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
 
 </head>
-<body>
-<div class="header">
-        <h1>Registro  - Sis Universidad Martin Lutero Quilali"</h1>
-        <h3>Usuario:  <?php echo $_SESSION["username"] ?></h3>
-</div>
-<nav>
-    <ul>
-        <li class="active"><a href="inicio.view.php">Inicio</a> </li>
-        <li><a href="alumnos.view.php">Registro de Alumnos</a> </li>
-        <li><a href="docentes.view.php">Registro de docentes</a> </li>
-        <li><a href="listadoalumnos.view.php">Listado de Alumnos</a> </li>
-        <li><a href="listadodocentes.view.php">Listado de Docentes</a> </li>
-        <li><a href="notas.view.php">Registro de Notas</a> </li>
-        <li><a href="listadonotas.view.php">Consulta de Notas</a> </li>
-        
-        <li class="right"><a href="logout.php">Salir</a> </li>
 
-    </ul>
-</nav>
+<body class="d-flex flex-column min-vh-100">
+    
+    <div class="container">
+        <?php
+        if (isset($_GET['err'])) {
+            echo '<h3 class="alert alert-danger">ERROR: Usuario no autorizado</h3>';
+        }
+        ?>
+        <?php
+          include 'includes/nav.php'
+        ?>
 
-<div class="body">
-    <div class="panel">
             <h4>Registro de docentes</h4>
             <form method="post" class="form" action="procesardocente.php">
-                <label>Nombre docente</label><br>
-                <input type="text" required name="nombresDoc" maxlength="45">
-                <br>
-                <label>Apellido docente</label><br>
-                <input type="text" required name="apellidosDoc" maxlength="45">
-                <br><br>
-                <label>No de carnet</label><br>
-                <input type="text" name="numcarnetDoc">
-                <br><br>
-                <label>Sexo</label><br><input  type="radio" name="sexoDoc" value="M"> Masculino
-                <input type="radio" name="sexoDoc"  value="F"> Femenino
-                <br><br>
-<!--                 <label>Grado</label><br>
-                <select name="grado" required>
-                    <?php foreach ($grados as $grado):?>
-                        <option value="<?php echo $grado['id'] ?>"><?php echo $grado['nombre'] ?></option>
-                    <?php endforeach;?>
-                </select>
-                <br><br> -->
-<!--                 <label>Sección</label><br>
+                <div class="form-row">
+                    <div class="form-group  col-12 col-sm-6">
+                        <label>Nombre docente</label><br>
+                        <input  class="form-control" type="text" required name="nombresDoc" maxlength="45">
+                    </div>
+                    <div class="form-group  col-12 col-sm-6">
+                        <label>Apellido docente</label><br>
+                        <input class="form-control" type="text" required name="apellidosDoc" maxlength="45">
+                    </div>
+                    <div class="form-group  col-12 col-sm-6">
+                        <label>No de carnet</label><br>
+                        <input class="form-control" type="text" name="numcarnetDoc">
+                    </div>
 
-                    <?php foreach ($secciones as $seccion):?>
-                        <input type="radio" name="seccion" required value="<?php echo $seccion['id'] ?>">Sección <?php echo $seccion['nombre'] ?>
-                    <?php endforeach;?> -->
+                    <fieldset class="form-group">
+                    <div class="row">
+                        <legend class="col-form-label col-sm-2 pt-0">Sexo</legend>
+                        <div class="col-12 col-sm-6">
+                            <div class="custom-control custom-radio custom-control-inline">
+                                <input class="form-check-input" type="radio" name="sexoDoc" id="gridRadios1" value="M" checked>
+                                <label class="form-check-label" for="gridRadios1">
+                                    Masculino
+                                </label>
+                            </div>
+                            <div class="custom-control custom-radio custom-control-inline">
+                                <input class="form-check-input" type="radio" name="sexoDoc" id="gridRadios2" value="F">
+                                <label class="form-check-label" for="gridRadios2">
+                                    Femenino
+                                </label>
+                            </div>
+                        </div>
+                </fieldset>
 
-                <br><br>
-                <button type="submit" name="insertarDoc">Guardar</button> <button type="reset">Limpiar</button> <a class="btn-link" href="listadodocentes.view.php">Ver Listado</a>
-                <br><br>
-                <!--mostrando los mensajes que recibe a traves de los parametros en la url-->
-                <?php
-                if(isset($_GET['err']))
-                    echo '<span class="error">Error al almacenar el registro</span>';
-                if(isset($_GET['info']))
-                    echo '<span class="success">Registro almacenado correctamente!</span>';
-                ?>
+<!--                     <div class="form-group  col-12 col-sm-6">
+                        <label>Sexo</label><br><input type="radio" name="sexoDoc" value="M"> Masculino
+                        <input type="radio" name="sexoDoc" value="F"> Femenino
+                    </div>
+                </div> -->
 
+                    <br><br>
+                    
+                    <br><br>
+                    <!--mostrando los mensajes que recibe a traves de los parametros en la url-->
+                    <?php
+                    if (isset($_GET['err']))
+                        echo '<span class="error">Error al almacenar el registro</span>';
+                    if (isset($_GET['info']))
+                        echo '<span class="success">Registro almacenado correctamente!</span>';
+                    ?>
+            </div>
+            <button type="submit" name="insertarDoc" class="btn btn-success">Guardar</button> <button class="btn btn-danger" type="reset">Limpiar</button> <a class="btn btn-info" href="listadodocentes.view.php">Ver Listado</a>
             </form>
-        <?php
-        if(isset($_GET['err']))
-            echo '<span class="error">Error al guardar</span>';
-        ?>
-        </div>
+            <?php
+            if (isset($_GET['err']))
+                echo '<span class="error">Error al guardar</span>';
+            ?>
+    </div>
 </div>
 
-<footer>
-    <p>Derechos reservados &copy; 2020</p>
-</footer>
+<?php
+    include 'includes/footer.php';
+    ?>
 
 </body>
 
